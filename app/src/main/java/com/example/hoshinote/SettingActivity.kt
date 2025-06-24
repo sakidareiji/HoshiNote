0abc433e10a1fb17d3977547c83a78356a64ec48
Reiji Sakida <c1318186@st,kanazawa-it.ac.jp>
2025-06-24 13:34:04 +0900

    目標設定アプリの作成

---
+++ app/src/main/java/com/example/hoshinote/SettingActivity.kt
@@ -0,0 +1,100 @@
+package com.example.hoshinote
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import com.example.hoshinote.ui.components.CustomHeader
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.hoshinote.ui.theme.HoshiNoteTheme
+import androidx.compose.material3.Scaffold
+
+class SettingActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HoshiNoteTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ){
+                    SettingScreen {
+                        finish()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingScreen(onBackClick: () -> Unit = {}) {
+    var username by remember { mutableStateOf("") }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CustomHeader(
+                showBackButton = true,
+                onBackClick = onBackClick
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .background(color = Color(0xFA0B0E21))
+            ) {
+                Text(
+                    text = "ユーザーネーム変更",
+                    fontSize = 13.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFFB0BEC5),
+                )
+                OutlinedTextField(
+                    value = username,
+                    onValueChange = { username = it },
+                    label = { Text("ユーザーネーム") },
+                    modifier = Modifier.fillMaxWidth().padding(bottom = 14.dp),
+                    shape = RoundedCornerShape(8.dp),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color(0xFFB0BEC5),
+                        unfocusedBorderColor = Color(0xFFB0BEC5),
+                        focusedLabelColor = Color(0xFFB0BEC5),
+                        unfocusedLabelColor = Color(0xFFB0BEC5),
+                        focusedContainerColor = Color(0xFF0B0E21),
+                        unfocusedContainerColor = Color(0xFF0B0E21),
+                        focusedTextColor = Color(0xFFB0BEC5),
+                        unfocusedTextColor = Color(0xFFB0BEC5)
+                    )
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hoshinote/ui/components/CustomHeader.kt
@@ -0,0 +1,76 @@
+package com.example.hoshinote.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.hoshinote.R
+
+@Composable
+fun CustomHeader(
+    onSettingsClick: () -> Unit = {},
+    onBackClick: (() -> Unit)? = null,
+    showBackButton: Boolean = false
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .statusBarsPadding()
+            .height(56.dp)
+            .background(Color(0xd00B0E21))
+            .padding(horizontal = 16.dp)
+
+    ) {
+        if (showBackButton && onBackClick != null) {
+            IconButton(
+                onClick = onBackClick,
+                modifier = Modifier.align(Alignment.CenterStart)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "戻る",
+                    tint = Color.White
+                )
+            }
+        }
+        
+        Text(
+            text = "HoshiNote",
+            color = Color.White,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.align(Alignment.Center)
+        )
+        
+        if (!showBackButton) {
+            IconButton(
+                onClick = onSettingsClick,
+                modifier = Modifier.align(Alignment.CenterEnd)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Settings,
+                    contentDescription = "設定",
+                    tint = Color.White
+                )
+            }
+        }
+    }
+}
