cf4780bb50e74621836cc652162c3e96b2910330
Reiji Sakida <c1318186@st,kanazawa-it.ac.jp>
2025-06-26 16:18:18 +0900

    重複したファイルの削除

--- app/src/main/java/com/example/hoshinote/ui/theme/Color.kt
+++ app/src/main/java/com/example/hoshinote/ui/theme/Color.kt
@@ -10,7 +10,6 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-// アプリ固有の色定義
 val DarkBlue = Color(0xFF0B0E21)
 val DarkBlueTransparent = Color(0xd00B0E21)
 val DarkBlueLightTransparent = Color(0xFA0B0E21)
