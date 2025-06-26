e319d10b09f6caab980a673fe507aee186bd431d
Reiji Sakida <c1318186@st,kanazawa-it.ac.jp>
2025-06-26 14:36:48 +0900

    sealed   classベースのルート定義に変更

---
+++ app/src/main/java/com/example/hoshinote/navigation/Routes.kt
@@ -0,0 +1,18 @@
+package com.example.hoshinote.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed class Routes {
+    @Serializable
+    data object UserRegistration : Routes()
+    
+    @Serializable
+    data object GoalSetting : Routes()
+    
+    @Serializable
+    data object GoalManagement : Routes()
+    
+    @Serializable
+    data object Setting : Routes()
+}
