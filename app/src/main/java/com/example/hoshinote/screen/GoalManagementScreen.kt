a690751a490224745add0544ea5b885c5af874a2
Reiji Sakida <c1318186@st,kanazawa-it.ac.jp>
2025-07-01 12:03:32 +0900

    コンフリクト対応

--- app/src/main/java/com/example/hoshinote/screen/GoalManagementScreen.kt
+++ app/src/main/java/com/example/hoshinote/screen/GoalManagementScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.hoshinote.components.CustomHeader
-import com.example.hoshinote.navigation.Routes
 import com.example.hoshinote.R
 
 @Composable
